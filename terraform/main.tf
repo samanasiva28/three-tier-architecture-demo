6e42028ea99312b2020567edbde52d3741b81e5e
samanasiva28 <samanasiva2@gmail.com>
2025-04-10 17:59:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+}
+
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
